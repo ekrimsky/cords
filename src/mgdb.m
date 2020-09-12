702f94f4dfe6a57efa1ac2b0b30b72195394f8aa
erez <ekrimsky@stanford.edu>
2020-09-11 19:40:52 -0700

    adds filters and robustnesss to database class

--- src/mgdb.m
+++ src/mgdb.m
@@ -28,14 +28,14 @@ classdef mgdb < matlab.mixin.Copyable
 %
 properties (GetAccess = public, SetAccess = private)
 
-
+    % NOTE: may want some of these to be private get access too
     motors          % containers.Map
     gearboxes       % containers.Map
     compatibility 
 
     verbosity
 
-    dd_key
+    dd_key   
     %
     %
     %       Filter Cutoffs -- NOTE: allow direct access or no? 
@@ -53,6 +53,8 @@ methods (Access = public)
 
 
     function obj = mgdb(varargin) %
+
+        init_tic = tic; 
 
         % TODO --- accouint for input redirecting us to a new search path 
         % If no args look in a a default location
@@ -89,7 +91,7 @@ methods (Access = public)
                 motor_files = [motor_files, m_tmp];
                 gearbox_files = [gearbox_files, g_tmp];
                 compat_files = [compat_files, c_tmp];
-            end 
+            end
         end 
 
         % Read in motor csvs, NOTE move to own function takinfg in list 
@@ -102,11 +104,12 @@ methods (Access = public)
         obj.motors = containers.Map(motor_keys, motor_values);
         obj.gearboxes = containers.Map(gb_keys, gb_values);
         % add the 'dummy' gearbox for a direct drive option (compatible with all motors)
-        direct_drive = struct('Key', 'DD', 'Manufacturer', 'none', 'ID', 'DD',...
-                        'stages', 0, 'ratio', 1, 'mass', 0, 'inerita', 0,...
-                        'efficiency', 1, 'direction', 1,,...
+        direct_drive = struct('key', 'DD', 'manufacturer', 'none',...
+                         'ID', 'DD', 'type', 'none',...
+                        'stages', 0, 'ratio', 1, 'mass', 0, 'inertia', 0,...
+                        'efficiency', 1, 'direction', 1,...
                         'max_int_torque', inf, 'max_cont_torque', inf);  
-        obj.gearboxes('DD', direct_drive); % add direct drive option to the map 
+        obj.gearboxes('DD') = direct_drive; % add direct drive option to the map 
 
         obj.compatibility = containers.Map('KeyType', 'char', 'ValueType', 'any'); % value type will be other maps 
         % Read in compatibility csvs 
@@ -119,24 +122,39 @@ methods (Access = public)
         %  filters is a struct where each field is different criteria
         %  initialized to not filter out any possible choices 
         % 
-
-
-
+        filters.omega_max = 0;      % max speed 
+        filters.omega_rms = 0; 
+        filters.tau_rms = 0;    % rms output torque 
+        filters.tau_max = 0;    % max output torque 
+        filters.total_mass = inf; 
+        filters.effective_inertia = inf; 
+        obj.filters = filters;
         %
         %           Save Object Out So Can Be Loaded by motor selection 
         %   
+
+
+        init_time = toc(init_tic); 
+        fprintf('Added %d motors and %d gearboxes to database in %0.2f seconds\n',...
+                    obj.motors.Count, obj.gearboxes.Count, init_time); 
 
     end 
 
 
 
     function [motor_combo_keys, gearbox_combo_keys] = get_combinations(obj)
+    %
+    %
+    %   Return structs instead of keys. This is less space efficient but it 
+    %   it makes it easier for the caller to use the outputs 
+    %
+    %
 
         % get motor, gearbox key tuples 
         motor_keys = obj.motors.keys; 
 
         motor_combo_keys = {}; % will be a cell array of motor keys 
-        gearbox_combo_key = {}; % will be a cell array of gb keys 
+        gearbox_combo_keys = {}; % will be a cell array of gb keys 
 
         % NOTE: may want to allocate pretty big then trim 
         cost_list = {}; % will be used to reindex and sort outputs 
@@ -145,14 +163,14 @@ methods (Access = public)
         % For each motor, get all compatible gearboxes 
         for m_idx = 1:length(motor_keys)
             motor_key = motor_keys{m_idx}; 
-            tmp_map = obj.compatibility(motor_key)
+            tmp_map = obj.compatibility(motor_key);
             gb_keys = tmp_map.keys; 
-            for gb_idx = 1:lengh(gb_keys) 
+            for gb_idx = 1:length(gb_keys) 
                 gb_key = gb_keys{gb_idx};
                 % Check if combo is valid given the filters
                 if obj.valid_combo(motor_key, gb_key)
                     motor_combo_keys{end + 1} = motor_key;
-                    gearbox_combo_key{end + 1} = gb_key; 
+                    gearbox_combo_keys{end + 1} = gb_key; 
                     cost_list{end + 1} = tmp_map(gb_key);
                 end 
                 init_combos = init_combos + 1; % to keep track of how many removed 
@@ -169,9 +187,22 @@ methods (Access = public)
         costs = cell2mat(cost_list); 
         [~, reindex] = sort(costs);
 
-        % Sort by rankings 
+        % Sort by rankings -- reindex keys much faster than reindexing structs 
         motor_combo_keys = motor_combo_keys(reindex);
         gearbox_combo_keys = gearbox_combo_keys(reindex); 
+
+
+        % Now turn into structs - this can be slow and not particularly space
+        % efficient -- loop faster than cellfun
+        %motor_struct = cellfun(@(x) obj.motors(x), motor_combo_keys)
+        for i = 1:length(motor_combo_keys)  % slightly faster than cell fun
+            motor_struct(i) = obj.motors(motor_combo_keys{i});
+            gearbox_struct(i) = obj.gearboxes(gearbox_combo_keys{i});
+        end 
+
+        % Returning combo indices would be more space efficient but its 
+        % not a robust way to handle things 
+
     end 
 
     function update_rankings(obj, motor_keys, gearbox_keys, new_costs)
@@ -183,7 +214,7 @@ methods (Access = public)
             motor_key = motor_keys{i};
             if obj.compatibility.isKey(motor_key)
                 tmp_map = obj.compatibility(motor_key);  % by reference 
-                tmp_map(gearbox_keys(i)) = new_costs(i);
+                tmp_map(gearbox_keys{i}) = new_costs(i);
             else 
                 error('Invalid motor key');
             end 
@@ -202,6 +233,12 @@ methods (Access = public)
 
 
     function update_filter(obj, criteria, value)
+        assert(value >= 0, 'update_filter: values must be nonnegative'); 
+        if isfield(obj.filters, criteria)
+            obj.filters.(criteria) = value;
+        else 
+            error('update_filter: invalid filter criteria');
+        end 
 
     end 
 
@@ -209,6 +246,32 @@ end
 
 
 methods (Access = private)
+
+    function [valid] = valid_combo(obj, motor_key, gearbox_key)
+
+        % Check this combination of motor and gearbox against the filters 
+        motor = obj.motors(motor_key);
+        gearbox = obj.gearboxes(gearbox_key);
+
+        % 
+        valid = true;
+        if (obj.filters.omega_max*gearbox.ratio) > motor.max_int_speed
+            valid = false; 
+        elseif (obj.filters.omega_rms*gearbox.ratio) > motor.max_cont_speed
+            valid = false; 
+        elseif obj.filters.tau_max > gearbox.max_int_torque
+            valid = false;
+        elseif obj.filters.tau_rms > gearbox.max_cont_torque
+            valid = false;
+        elseif motor.mass + gearbox.mass > obj.filters.total_mass 
+            valid = false; 
+        elseif (motor.inertia*gearbox.inertia)*(gearbox.ratio^2) ...
+                                    > obj.filters.effective_inertia
+            valid = false;
+        end 
+
+                    
+    end 
 
 
     function add_compatibility(obj, compat_files)
@@ -301,23 +364,20 @@ methods (Access = private)
 
 
 
-    function [valid] = valid_combo(obj, motor_key, gearbox_key)
 
-        % Check this combination of motor and gearbox against the filters 
-        motor = obj.motors(motor_key);
-        gearbox = obj.gearboxes(gearbox_key); 
 
-        % TODO -- actually check against the filters 
-
-        % total mass 
-        % effective inertia (through gb ratio)
-        % max_int_torque (check gearbox then motor)
-        % max_vel (gearbox and motor) 
-        % max_cont_power
-        valid = true;
+    function vprintf(obj, priority, varargin)
+    %
+    %
+    %
+    %    
+        if priority >= obj.settings.verbose
+            fprintf(varargin{:}); 
+        end 
     end 
 
-end 
+
+end % end private methods 
 
 %=================== END CLASS DEF =========================
 end 
@@ -354,14 +414,14 @@ end
 function [motor_keys, motor_cells] = read_motor_file(motor_file)
     T = readtable(motor_file);    % NOTE: read table can be slow - may be able to speed up by specifing delims and other things
     motor_struct = table2struct(T);
-    motor_keys = {motor_struct(:).Key};
+    motor_keys = {motor_struct(:).key};
     motor_cells = num2cell(motor_struct); 
 end 
 
 function [gearbox_keys, gearbox_cells] = read_gearbox_file(gearbox_file)
     T = readtable(gearbox_file);
     gearbox_struct = table2struct(T);
-    gearbox_keys = {gearbox_struct(:).Key};
+    gearbox_keys = {gearbox_struct(:).key};
     gearbox_cells = num2cell(gearbox_struct); 
 end 
 
