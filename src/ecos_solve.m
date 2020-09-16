b43c20608e0ce39049aee566d8f80818f61790f9
erez <ekrimsky@stanford.edu>
2020-09-16 00:19:39 -0700

    adds support for rotated SOC constraints (standard SOC constraints not yet tested)

--- src/ecos_solve.m
+++ src/ecos_solve.m
@@ -18,6 +18,8 @@ function result = ecos_solve(c, A_eq, b_eq, A_lp, b_lp, G_soc, h_soc)
 %       Stanford University, Biomechatronic Lab 
 %*******************************************************************************
 
+%TODO -- settings
+%constrinats 
 
 dims.l = size(A_lp, 1); 
 dims.q = zeros(numel(G_soc), 1);
@@ -32,8 +34,9 @@ h = [b_lp; cat(1, h_soc{:})];
 % TODO -- incorporate more detailed settinsg into caller
 % Potnetially link verbosity as well -- add as input 
 opts =  ecosoptimset('VERBOSE', 0,...
-                     'FEASTOL', 1e-4,...
-                      'ABSTOL', 1e-4);
+                      'maxit', 100,... % default 50 
+                     'FEASTOL', 1e-5,...  % bigger can lead to issues
+                     'ABSTOL', 5e-4);  % smaller can lead to issues
 
 [x, y, data, s, z] = ecos(c, G, h, dims, A_eq, b_eq, opts); 
 
@@ -42,17 +45,34 @@ opts =  ecosoptimset('VERBOSE', 0,...
 
 result.x = x(1:end-1);   % NOTE: some dual info could be good, think on how to integrate this better 
 
-if data.exitflag == 0
+% Exits Flags
+% -1 : iteration limit 
+% 0 : good sol
+% 1 : Ceritificate of infeas
+% 10 : Optimial solution found within reduced tolerance 
+
+result.objval = inf;
+if (data.exitflag == 0) 
     result.objval = data.pcost; 
-else 
-    %data.exitflag == 2
-    result.objval = inf; 
+elseif (data.exitflag == 10) 
+    result.objval = data.pcost; 
+    display(data)
+elseif (data.exitflag == 1) 
+    %  % infeasibility proven 
+elseif data.exitflag == -2 
+    display(data)
+    warning(['ecos_solve:Numerical issues may have been encountered. ',...
+                                     'Consider loosening ABSTOL']);
+    keyboard
+else
+    display(data)
 end 
 %else % TODO -- check 
 %    keyboard%
 %
 %end 
 
+% 1 - certi of primal infeasibility 
 
 
 result.runtime = data.timing.runtime; % TODO -- comaptibility 
